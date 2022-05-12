58eaa45d5f2c2c26a7f13a64a0cfae99feb4f084
hanzzoo <kou435@gmail.com>
2022-05-13 08:01:06 +0900

    feat: create counterSlice for sample

--- app/store.ts
+++ app/store.ts
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import sampleReducer from "../features/sampleSlice";
+import counterReducer from "../features/counterSlice";
 
 export const store = configureStore({
   reducer: {
-    sample: sampleReducer,
+    sample: counterReducer,
   },
 });
 
