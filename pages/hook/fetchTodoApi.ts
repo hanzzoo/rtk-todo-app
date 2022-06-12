3c52a31dc59449abd47dddcb188fae2a0976c24b
hanzzoo <kou435@gmail.com>
2022-06-12 14:09:10 +0900

    feat: create onPostAPI func and thunk

--- pages/hook/fetchTodoApi.ts
+++ pages/hook/fetchTodoApi.ts
@@ -1,14 +1,25 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-type Method = "GET";
-const API_ROOT_URL = "https://jsonplaceholder.typicode.com/todos";
-const PARAMS = "?userId=1";
+const API_ROOT_URL = "https://jsonplaceholder.typicode.com";
+const USER_QUERY = "?userId=1";
 
 const fetchTodoApi = async () => {
   try {
-    const data = await axios.get(`${API_ROOT_URL}${PARAMS}`);
-    return data;
+    const getData = await axios.get(`${API_ROOT_URL}/todos${USER_QUERY}`);
+    return getData;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const onPostAPI = async (query: string) => {
+  try {
+    const postData = await axios.post(`${API_ROOT_URL}/posts`, {
+      title: query,
+      userId: 1,
+    });
+    return postData;
   } catch (error) {
     console.log(error);
   }
@@ -22,3 +33,15 @@ export const fetchTodoThunk = createAsyncThunk("fetch/todo", async () => {
     console.error(error);
   }
 });
+
+export const onPostTodoThunk = createAsyncThunk(
+  "post/todo",
+  async (query: string) => {
+    try {
+      const postData = await onPostAPI(query);
+      return postData;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
